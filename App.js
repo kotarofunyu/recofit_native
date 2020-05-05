e76b494b40eedbdef61dbb6e4b6510eb4ab2dbb2
kotarofunyu <kotarofunyu@gmail.com>
2020-05-06 01:50:37 +0900

    ログイン機能を仮で実装

--- App.js
+++ App.js
@@ -6,13 +6,24 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import HomeScreen from './src/screens/HomeScreen';
 import EventScreen from './src/screens/EventScreen';
 import UserScreen from './src/screens/UserScreen';
+import MainScreen from './src/screens/MainScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 Ionicons.loadFont();
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const MainNavigator = createAppContainer(
+    createSwitchNavigator({
+      login: { screen: LoginScreen },
+      main: { screen: MainScreen }
+    })
+  )
+
   return (
     <NavigationContainer>
+      <MainNavigator />
       <Tab.Navigator
         screenOptions={({route}) => ({
           tabBarIcon: ({focused, color, size}) => {
@@ -34,9 +45,11 @@ export default function App() {
           activeTintColor: '#ffa500',
           inactiveTintColor: 'grey',
         }}>
+        <Tab.Screen name="メイン" component={MainScreen} />
         <Tab.Screen name="ホーム" component={HomeScreen} />
         <Tab.Screen name="種目" component={EventScreen} />
         <Tab.Screen name="ユーザー" component={UserScreen} />
+        <Tab.Screen name="ログイン" component={LoginScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
--- src/screens/UserScreen.js
+++ src/screens/UserScreen.js
@@ -5,12 +5,14 @@ import {
   StyleSheet,
 } from 'react-native';
 import Title from '../elements/Header';
+import Heading from '../elements/Heading';
 
 class UserScreen extends React.Component {
   render() {
     return (
       <View>
         <Title />
+        <Heading name="ユーザー" />
         <Text>
           ユーザー一覧
         </Text>
