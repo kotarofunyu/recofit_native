efa7decba2635669d3035b7219d01cceb6997c27
kotarofunyu <kotarofunyu@gmail.com>
2020-05-06 02:51:17 +0900

    ログイン中のユーザーを表示させることに成功

--- src/screens/UserScreen.js
+++ src/screens/UserScreen.js
@@ -3,18 +3,51 @@ import {
   View,
   Text,
   StyleSheet,
+  AsyncStorage,
 } from 'react-native';
 import Title from '../elements/Header';
 import Heading from '../elements/Heading';
+import { FlatList } from 'react-native-gesture-handler';
+
 
 class UserScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { taskName: '', tasks:[], loading: '', apiToken: ''};
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true, apiToken: await AsyncStorage.getItem('api_token') })
+    fetch(`http://localhost/api/user/${this.state.apiToken}`)
+    .then((response) => response.json())
+    .then((jsonData) => (this.setState({ loading: false, tasks: jsonData })))
+    .catch((error) => console.error(error));
+  }
+
+  // renderTasks() {
+  //   if (this.state.loading) return <FlatList />
+  //   else {
+  //     return (
+  //       <FlatList
+  //       data={this.state.tasks}
+  //       keyExtractor={(item) => item.id.toString()}
+  //       renderItem={({item}) => (
+  //         <CheckBox title={item.name} 
+  //       )}
+  //     )
+  //   }
+  // }
+
   render() {
     return (
       <View>
         <Title />
-        <Heading name="ユーザー" />
+        <Heading name="ログイン中のユーザー" />
         <Text>
-          ユーザー一覧
+          { this.state.tasks.name }
+        </Text>
+        <Text>
+          { this.state.tasks.introduction }
         </Text>
       </View>
     )
