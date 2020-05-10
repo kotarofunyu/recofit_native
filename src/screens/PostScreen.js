0c14cf07603ec3a5ceb6b3e8d5398cbe58140a77
kotarofunyu <kotarofunyu@gmail.com>
2020-05-10 11:25:46 +0900

    簡易的なフォームだけ作った

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -4,6 +4,24 @@ import Title from '../elements/Header';
 import Heading from '../elements/Heading';
 
 export default class PostScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comment: 'aaaaa',
+      eventData: [
+        {
+          name: '',
+          setData: {
+            weight: '20',
+            rep: '11',
+            set: '0',
+            renewal: null,
+            main: null,
+          },
+        },
+      ],
+    };
+  }
   submitButton() {
     if (this.state.loading) {
       return <ActivityIndicator size="small" />;
@@ -26,8 +44,26 @@ export default class PostScreen extends React.Component {
         <View>
           <TextInput
             placeholder="今日のトレーニングはどうだった？"
+            value={this.state.comment}
             onChangeText={comment => this.setState({comment})}
+            style={styles.textArea}
           />
+        </View>
+        <View>
+          <View style={styles.setArea}>
+            <TextInput
+              placeholder="重量kg"
+              value={this.state.eventData[0].weight}
+            />
+            <TextInput
+              placeholder="回数rep"
+              value={this.state.eventData[0].rep}
+            />
+            <TextInput
+              placeholder="セット"
+              value={this.state.eventData[0].set}
+            />
+          </View>
         </View>
         <View>
           <Text>種目を追加</Text>
@@ -37,3 +73,15 @@ export default class PostScreen extends React.Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  textArea: {
+    height: 50,
+    backgroundColor: '#308EEF',
+    margin: 10,
+  },
+  setArea: {
+    flexDirection: 'row',
+    marginTop: 20,
+  },
+});
